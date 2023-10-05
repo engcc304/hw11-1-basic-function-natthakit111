d0badb661cbe766f88b967351cee882837072cfd
natthakit111 <natthakit_ya66@live.ac.th>
2023-10-05 14:31:21 +0700

    update hw11-1

--- Homework/HW11/hw11-1-basic-function.cpp
+++ Homework/HW11/hw11-1-basic-function.cpp
@@ -1,5 +1,6 @@
 /*
-    รับค่าจากผู้ใช้จำนวน 3 ค่า และสร้างฟังก์ชันในการหาผลรวมของจำนวน 3 ค่านี้ และแสดงผลลัพธ์มายังหน้าจอ พร้อมระบุชื่อฟังก์ชันที่เรียกใช้งาน เช่น รับค่า 3, 5, 6 จะแสดงผลลัพธ์ว่า 14 (Calculate by Additional Function)
+    รับค่าจากผู้ใช้จำนวน 3 ค่า และสร้างฟังก์ชันในการหาผลรวมของจำนวน 3 ค่านี้ และแสดงผลลัพธ์มายังหน้าจอ พร้อมระบุชื่อฟังก์ชันที่เรียกใช้งาน 
+    เช่น รับค่า 3, 5, 6 จะแสดงผลลัพธ์ว่า 14 (Calculate by Additional Function)
 
     Test case:
         Input 1 : 
@@ -11,4 +12,32 @@
     Output:
         Summation = 12 (Calculate by Additional Function)
 */
+#include <stdio.h>
 
+int sum(int a, int b, int c) ;
+
+int main() {
+
+    int ans ;
+    int input1, input2, input3 ;
+
+    printf( "Input 1 :\n" ) ;
+    scanf( "%d", &input1 ) ;
+
+    printf( "Input 2 :\n" ) ;
+    scanf( "%d", &input2 ) ;
+
+    printf( "Input 3 :\n" ) ;
+    scanf( "%d", &input3 ) ;
+
+    ans = sum( input1, input2, input3 ) ;
+    
+    printf( "Summation = %d (Calculate by Additional Function)", ans ) ;
+
+    return 0 ;
+}//end function main
+
+int sum(int a, int b, int c) {
+
+    return a + b + c ;
+}//end function sum
